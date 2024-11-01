fbc2e926df85eb7935afdd78d441e06d60e8f8ba
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2024-11-01 10:43:00 +0300

    This is the updated content for the file run_experiment.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ from datasets import Dataset as HFDataset, DatasetDict as HF_DatasetDict
 from peft import LoraConfig, TaskType, get_peft_model, prepare_model_for_kbit_training
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import requests
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer
 from torch.utils.data import Dataset, DataLoader
 import torch
 
-# Disable SSL warnings
 def disable_ssl_warnings():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     original_request = requests.Session.request
@@ -24,11 +23,32 @@ def disable_ssl_warnings():
 
     requests.Session.request = patched_request
 
-# Set seed
-def set_seed(seed):
-    return partial(set_seed, seed)
+class ModelArguments:
+    model_name_or_path: str = field(metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"})
+    chat_template_format: Optional[str] = field(default="none", metadata={"help": "chatml|zephyr|none. Pass `none` if the dataset is already formatted with the chat template."})
+    lora_alpha: Optional[int] = field(default=16)
+    lora_dropout: Optional[float] = field(default=0.1)
+    lora_r: Optional[int] = field(default=64)
+    lora_target_modules: Optional[str] = field(default="q_proj,k_proj,v_proj,o_proj,down_proj,up_proj,gate_proj", metadata={"help": "comma separated list of target modules to apply LoRA layers to"})
+    use_nested_quant: Optional[bool] = field(default=False, metadata={"help": "Activate nested quantization for 4bit base models"})
+    bnb_4bit_compute_dtype: Optional[str] = field(default="float16", metadata={"help": "Compute dtype for 4bit base models"})
+    bnb_4bit_quant_storage_dtype: Optional[str] = field(default="uint8", metadata={"help": "Quantization storage dtype for 4bit base models"})
+    bnb_4bit_quant_type: Optional[str] = field(default="nf4", metadata={"help": "Quantization type fp4 or nf4"})
+    use_flash_attn: Optional[bool] = field(default=False, metadata={"help": "Enables Flash attention for training."})
+    use_peft_lora: Optional[bool] = field(default=False, metadata={"help": "Enables PEFT LoRA for training."})
+    use_8bit_quantization: Optional[bool] = field(default=False, metadata={"help": "Enables loading model in 8bit."})
+    use_4bit_quantization: Optional[bool] = field(default=False, metadata={"help": "Enables loading model in 4bit."})
+    use_reentrant: Optional[bool] = field(default=False, metadata={"help": "Gradient Checkpointing param. Refer the related docs"})
+    use_unsloth: Optional[bool] = field(default=False, metadata={"help": "Enables UnSloth for training."})
+    use_triplet_loss_trainer: Optional[bool] = field(default=False, metadata={"help": "Use our TripletLossTrainer(Trainer)"})
 
-# Prepare model
+class DataTrainingArguments:
+    dataset_name: Optional[str] = field(default="timdettmers/openassistant-guanaco", metadata={"help": "The preference dataset to use."})
+    append_concat_token: Optional[bool] = field(default=False, metadata={"help": "If True, appends `eos_token_id` at the end of each sample being packed."})
+    add_special_tokens: Optional[bool] = field(default=False, metadata={"help": "If True, tokenizers adds special tokens to each sample being packed."})
+    splits: Optional[str] = field(default="train,test", metadata={"help": "Comma separate list of the splits to use from the dataset."})
+    tokenized_dataset_path: Optional[str] = field(default=None, metadata={"help": "Path to the tokenized dataset on disk."})
+
 def prepare_model(model_args, data_args, training_args):
     model = get_peft_model(model_args.model_name_or_path, LoraConfig(
         r=model_args.lora_r,
@@ -50,7 +70,6 @@ def prepare_model(model_args, data_args, training_args):
     model = prepare_model_for_kbit_training(model, peft_config)
     return model, peft_config
 
-# Create datasets
 def create_datasets(tokenizer, data_args, training_args, apply_chat_template):
     dataset = HFDataset.from_dataset(data_args.dataset_name, split=data_args.splits)
 
@@ -72,7 +91,6 @@ def create_datasets(tokenizer, data_args, training_args, apply_chat_template):
     dataset = dataset.map(process_data, batched=True)
     return HF_DatasetDict(dataset)
 
-# Triplet dataset
 class TripletDataset(Dataset):
     def __init__(self, dataset, epoch, batch_size):
         self.dataset = dataset
@@ -89,15 +107,6 @@ class TripletDataset(Dataset):
         negative_samples = [sample for sample in batch if sample["label"] == 0]
         return positive_samples, negative_samples
 
-# Get trainer
-def get_trainer(model, peft_config, train_dataset, eval_dataset, model_args, training_args):
-    if model_args.use_triplet_loss_trainer:
-        model = get_peft_model(model, peft_config)
-        return TripletLossTrainer(model=model, args=training_args, train_dataset=train_dataset, eval_dataset=eval_dataset, layer_index=-1)
-    else:
-        return SFTTrainer(model=model, tokenizer=model.tokenizer, args=training_args, train_dataset=train_dataset, eval_dataset=eval_dataset, peft_config=peft_config)
-
-# SFT trainer
 class SFTTrainer(Trainer):
     def __init__(self, model, tokenizer, args, train_dataset, eval_dataset, peft_config):
         super().__init__(model, args, train_dataset, eval_dataset)
@@ -116,7 +125,6 @@ class SFTTrainer(Trainer):
     def accelerator(self):
         return None
 
-# Triplet loss trainer
 class TripletLossTrainer(SFTTrainer):
     def __init__(self, model, args, train_dataset, eval_dataset, layer_index):
         super().__init__(model, model.tokenizer, args, train_dataset, eval_dataset, None)
@@ -134,7 +142,13 @@ class TripletLossTrainer(SFTTrainer):
     def accelerator(self):
         return None
 
-# Trainer pipeline
+def get_trainer(model, peft_config, train_dataset, eval_dataset, model_args, training_args):
+    if model_args.use_triplet_loss_trainer:
+        model = get_peft_model(model, peft_config)
+        return TripletLossTrainer(model=model, args=training_args, train_dataset=train_dataset, eval_dataset=eval_dataset, layer_index=-1)
+    else:
+        return SFTTrainer(model=model, tokenizer=model.tokenizer, args=training_args, train_dataset=train_dataset, eval_dataset=eval_dataset, peft_config=peft_config)
+
 class TrainerPipeline:
     def __init__(self, model_args, data_args, training_args):
         self.model_args = model_args
@@ -155,38 +169,6 @@ class TrainerPipeline:
         if trainer.is_fsdp_enabled:
             trainer.accelerator.state.fsdp_plugin.set_state_dict_type("FULL_STATE_DICT")
         trainer.save_model()
-
-# Model arguments
-@dataclass
-class ModelArguments:
-    """Model arguments data class."""
-    model_name_or_path: str = field(metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"})
-    chat_template_format: Optional[str] = field(default="none", metadata={"help": "chatml|zephyr|none. Pass `none` if the dataset is already formatted with the chat template."})
-    lora_alpha: Optional[int] = field(default=16)
-    lora_dropout: Optional[float] = field(default=0.1)
-    lora_r: Optional[int] = field(default=64)
-    lora_target_modules: Optional[str] = field(default="q_proj,k_proj,v_proj,o_proj,down_proj,up_proj,gate_proj", metadata={"help": "comma separated list of target modules to apply LoRA layers to"})
-    use_nested_quant: Optional[bool] = field(default=False, metadata={"help": "Activate nested quantization for 4bit base models"})
-    bnb_4bit_compute_dtype: Optional[str] = field(default="float16", metadata={"help": "Compute dtype for 4bit base models"})
-    bnb_4bit_quant_storage_dtype: Optional[str] = field(default="uint8", metadata={"help": "Quantization storage dtype for 4bit base models"})
-    bnb_4bit_quant_type: Optional[str] = field(default="nf4", metadata={"help": "Quantization type fp4 or nf4"})
-    use_flash_attn: Optional[bool] = field(default=False, metadata={"help": "Enables Flash attention for training."})
-    use_peft_lora: Optional[bool] = field(default=False, metadata={"help": "Enables PEFT LoRA for training."})
-    use_8bit_quantization: Optional[bool] = field(default=False, metadata={"help": "Enables loading model in 8bit."})
-    use_4bit_quantization: Optional[bool] = field(default=False, metadata={"help": "Enables loading model in 4bit."})
-    use_reentrant: Optional[bool] = field(default=False, metadata={"help": "Gradient Checkpointing param. Refer the related docs"})
-    use_unsloth: Optional[bool] = field(default=False, metadata={"help": "Enables UnSloth for training."})
-    use_triplet_loss_trainer: Optional[bool] = field(default=False, metadata={"help": "Use our TripletLossTrainer(Trainer)"})
-
-# Data training arguments
-@dataclass
-class DataTrainingArguments:
-    """Data training arguments data class."""
-    dataset_name: Optional[str] = field(default="timdettmers/openassistant-guanaco", metadata={"help": "The preference dataset to use."})
-    append_concat_token: Optional[bool] = field(default=False, metadata={"help": "If True, appends `eos_token_id` at the end of each sample being packed."})
-    add_special_tokens: Optional[bool] = field(default=False, metadata={"help": "If True, tokenizers adds special tokens to each sample being packed."})
-    splits: Optional[str] = field(default="train,test", metadata={"help": "Comma separate list of the splits to use from the dataset."})
-    tokenized_dataset_path: Optional[str] = field(default=None, metadata={"help": "Path to the tokenized dataset on disk."})
 
 def main():
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
