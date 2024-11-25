28257c9c773026c8daec185359dc47de14be9cb3
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2024-11-26 01:11:50 +0300

    This is the updated content for the file run_experiment.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class Hyperparameters:
     resume_checkpoint: str = None
     negative_samples: int = 5
 
-class TripletDataset(tf.keras.utils.Sequence):
+class Dataset:
     def __init__(self, data, conversation_format, negative_samples, batch_size):
         self.data = data
         self.conversation_format = conversation_format
@@ -96,58 +96,30 @@ class T5Model(models.Model):
         x = self.fc3(x)
         return x
 
-class TripletLoss(tf.keras.losses.Loss):
-    def __init__(self, margin=2.0):
-        super(TripletLoss, self).__init__()
-        self.margin = margin
-
-    def call(self, anchor, positive, negative):
-        distance_positive = tf.reduce_sum(tf.square(anchor - positive), axis=1)
-        distance_negative = tf.reduce_sum(tf.square(anchor - negative), axis=1)
-        losses = tf.maximum(distance_positive - distance_negative + self.margin, 0)
-        return tf.reduce_mean(losses)
-
-class ModelTrainer:
-    def __init__(self, model, hyperparameters):
-        self.model = model
-        self.hyperparameters = hyperparameters
-        self.optimizer = optimizers.Adam(learning_rate=0.001)
-        self.loss_fn = TripletLoss()
+    def compile(self, optimizer, loss_fn, metrics):
+        super(T5Model, self).compile()
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.metrics = metrics
 
     def train_step(self, batch):
         with tf.GradientTape() as tape:
             anchor_input_ids = batch["input_ids"]
             positive_input_ids = batch["labels"]
             negative_input_ids = batch["negative_examples"][0]["input_ids"]
-            anchor_outputs = self.model(anchor_input_ids, training=True)
-            positive_outputs = self.model(positive_input_ids, training=True)
-            negative_outputs = self.model(negative_input_ids, training=True)
+            anchor_outputs = self(anchor_input_ids, training=True)
+            positive_outputs = self(positive_input_ids, training=True)
+            negative_outputs = self(negative_input_ids, training=True)
             loss = self.loss_fn(anchor_outputs, positive_outputs, negative_outputs)
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         return loss
 
-    def train(self, dataset):
-        for epoch in range(self.hyperparameters.num_epochs):
-            total_loss = 0
-            for batch in dataset:
-                loss = self.train_step(batch)
-                total_loss += loss
-            print(f"Epoch {epoch+1}, Loss: {total_loss / len(dataset)}")
-
-    def evaluate(self, dataset):
-        total_loss = 0
-        for batch in dataset:
-            input_ids = batch["input_ids"]
-            labels = batch["labels"]
-            attention_mask = batch["attention_mask"]
-            outputs = self.model(input_ids)
-            loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)(labels, outputs)
-            total_loss += loss
-        print(f"Test Loss: {total_loss / len(dataset)}")
-
-    def save_model(self, epoch):
-        self.model.save_weights(f"{self.hyperparameters.output_dir}/model_{epoch}.h5")
+def triplet_loss(anchor, positive, negative, margin=2.0):
+    distance_positive = tf.reduce_sum(tf.square(anchor - positive), axis=1)
+    distance_negative = tf.reduce_sum(tf.square(anchor - negative), axis=1)
+    losses = tf.maximum(distance_positive - distance_negative + margin, 0)
+    return tf.reduce_mean(losses)
 
 def load_data(file_name):
     try:
@@ -157,19 +129,41 @@ def load_data(file_name):
         print(f"{file_name} not found.")
         return None
 
+def train(model, dataset, hyperparameters):
+    for epoch in range(hyperparameters.num_epochs):
+        total_loss = 0
+        for batch in dataset:
+            loss = model.train_step(batch)
+            total_loss += loss
+        print(f"Epoch {epoch+1}, Loss: {total_loss / len(dataset)}")
+
+def evaluate(model, dataset):
+    total_loss = 0
+    for batch in dataset:
+        input_ids = batch["input_ids"]
+        labels = batch["labels"]
+        attention_mask = batch["attention_mask"]
+        outputs = model(input_ids)
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)(labels, outputs)
+        total_loss += loss
+    print(f"Test Loss: {total_loss / len(dataset)}")
+
+def save_model(model, epoch, output_dir):
+    model.save_weights(f"{output_dir}/model_{epoch}.h5")
+
 def main():
     hyperparameters = Hyperparameters(model_base="t5-base", conversation_format="none", triplet_loss_training=True)
     model = T5Model()
     model.build(input_shape=(None, 224, 224, 3))
+    model.compile(optimizer=optimizers.Adam(learning_rate=0.001), loss_fn=triplet_loss, metrics=['accuracy'])
     training_data, testing_data = load_data("train.json"), load_data("test.json")
     if training_data is not None and testing_data is not None:
-        train_dataset = TripletDataset(training_data, hyperparameters.conversation_format, hyperparameters.negative_samples, hyperparameters.train_batch_size)
-        test_dataset = TripletDataset(testing_data, hyperparameters.conversation_format, hyperparameters.negative_samples, hyperparameters.eval_batch_size)
-        trainer = ModelTrainer(model, hyperparameters)
+        train_dataset = Dataset(training_data, hyperparameters.conversation_format, hyperparameters.negative_samples, hyperparameters.train_batch_size)
+        test_dataset = Dataset(testing_data, hyperparameters.conversation_format, hyperparameters.negative_samples, hyperparameters.eval_batch_size)
         for epoch in range(hyperparameters.num_epochs):
-            trainer.train(train_dataset)
-            trainer.evaluate(test_dataset)
-            trainer.save_model(epoch)
+            train(model, train_dataset, hyperparameters)
+            evaluate(model, test_dataset)
+            save_model(model, epoch, hyperparameters.output_dir)
 
 if __name__ == "__main__":
     main()
