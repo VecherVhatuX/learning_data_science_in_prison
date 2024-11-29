abdffa2bf4e13078fa3f10add73bb8472039dc19
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2024-11-29 11:46:55 +0300

    This is the updated content for the file run_experiment.py

--- our_trainers.py
+++ our_trainers.py
@@ -5,41 +5,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
-# Functional style model definition
-def create_model(embedding_dim, num_features):
-    inputs = keras.Input(shape=(10,), dtype='int32')
-    x = layers.Embedding(embedding_dim, num_features)(inputs)
-    x = layers.GlobalAveragePooling1D()(x)
-    x = layers.Flatten()(x)
-    x = layers.Dense(num_features)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LayerNormalization()(x)
-    return keras.Model(inputs=inputs, outputs=x)
+class TripletModel(keras.Model):
+    def __init__(self, embedding_dim, num_features):
+        super(TripletModel, self).__init__()
+        self.embedding = layers.Embedding(embedding_dim, num_features)
+        self.global_average_pooling = layers.GlobalAveragePooling1D()
+        self.flatten = layers.Flatten()
+        self.dense = layers.Dense(num_features)
+        self.batch_normalization = layers.BatchNormalization()
+        self.layer_normalization = layers.LayerNormalization()
 
-# Functional style loss function definition
-def triplet_loss(y_true, y_pred, margin=1.0):
-    anchor, positive, negative = y_pred
-    d_ap = tf.norm(anchor - positive, axis=-1)
-    d_an = tf.norm(anchor[:, tf.newaxis, :] - negative, axis=-1)
-    loss = tf.maximum(d_ap - tf.reduce_min(d_an, axis=-1) + margin, tf.zeros_like(d_ap))
-    return tf.reduce_mean(loss)
+    def call(self, inputs):
+        x = self.embedding(inputs)
+        x = self.global_average_pooling(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = self.batch_normalization(x)
+        x = self.layer_normalization(x)
+        return x
 
-# Functional style dataset generator definition
-def create_dataset(samples, labels, num_negatives, batch_size):
-    def generator():
-        while True:
-            anchor_idx = np.random.choice(len(samples), size=batch_size, replace=False)
-            anchor_label = labels[anchor_idx]
-            positive_idx = np.array([np.random.choice(np.where(labels == label)[0], size=1)[0] for label in anchor_label])
-            negative_idx = np.array([np.random.choice(np.where(labels != label)[0], size=num_negatives, replace=False) for label in anchor_label])
-            yield samples[anchor_idx], samples[positive_idx], samples[negative_idx]
-    return generator
+class TripletLoss(keras.losses.Loss):
+    def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
 
-# Functional style training function definition
+    def call(self, y_true, y_pred):
+        anchor, positive, negative = y_pred
+        d_ap = tf.norm(anchor - positive, axis=-1)
+        d_an = tf.norm(anchor[:, tf.newaxis, :] - negative, axis=-1)
+        loss = tf.maximum(d_ap - tf.reduce_min(d_an, axis=-1) + self.margin, tf.zeros_like(d_ap))
+        return tf.reduce_mean(loss)
+
+class TripletDataset(tf.keras.utils.Sequence):
+    def __init__(self, samples, labels, num_negatives, batch_size):
+        self.samples = samples
+        self.labels = labels
+        self.num_negatives = num_negatives
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return len(self.samples) // self.batch_size
+
+    def __getitem__(self, idx):
+        anchor_idx = np.random.choice(len(self.samples), size=self.batch_size, replace=False)
+        anchor_label = self.labels[anchor_idx]
+        positive_idx = np.array([np.random.choice(np.where(self.labels == label)[0], size=1)[0] for label in anchor_label])
+        negative_idx = np.array([np.random.choice(np.where(self.labels != label)[0], size=self.num_negatives, replace=False) for label in anchor_label])
+        return self.samples[anchor_idx], self.samples[positive_idx], self.samples[negative_idx]
+
 def train(model, dataset, epochs, optimizer):
-    loss_fn = lambda y_true, y_pred: triplet_loss(y_true, y_pred)
+    loss_fn = TripletLoss()
     for epoch in range(epochs):
-        for batch in dataset():
+        for batch in dataset:
             with tf.GradientTape() as tape:
                 anchor, positive, negative = batch
                 anchor_embeddings = model(anchor)
@@ -50,7 +67,6 @@ def train(model, dataset, epochs, optimizer):
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
             print(f'Epoch: {epoch+1}, Loss: {loss.numpy()}')
 
-# Functional style validation function definition
 def validate(model, embeddings, labels, k=5):
     predicted_embeddings = model(embeddings)
     print("Validation KNN Accuracy:", knn_accuracy(predicted_embeddings, labels, k))
@@ -59,24 +75,21 @@ def validate(model, embeddings, labels, k=5):
     print("Validation KNN F1-score:", knn_f1(predicted_embeddings, labels, k))
     embedding_visualization(predicted_embeddings, labels)
 
-# Functional style distance calculation function definition
 def calculate_distances(output):
     print(tf.norm(output - output, axis=-1).numpy())
     print(tf.reduce_sum(output * output, axis=-1) / (tf.norm(output, axis=-1) * tf.norm(output, axis=-1)).numpy())
     print(1 - tf.reduce_sum(output * output, axis=-1) / (tf.norm(output, axis=-1) * tf.norm(output, axis=-1)).numpy())
 
-# Functional style nearest neighbors calculation function definition
 def calculate_neighbors(predicted_embeddings, output, k=5):
     print(tf.argsort(tf.norm(predicted_embeddings - output, axis=-1), direction='ASCENDING')[:, :k].numpy())
     print(tf.argsort(tf.reduce_sum(predicted_embeddings * output, axis=-1) / (tf.norm(predicted_embeddings, axis=-1) * tf.norm(output, axis=-1)), direction='DESCENDING')[:, :k].numpy())
 
-# Functional style pipeline definition
 def pipeline(learning_rate, batch_size, epochs, num_negatives, embedding_dim, num_features, size):
     samples = np.random.randint(0, 100, (size, 10))
     labels = np.random.randint(0, 2, (size,))
-    model = create_model(embedding_dim, num_features)
+    model = TripletModel(embedding_dim, num_features)
     optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-    dataset = create_dataset(samples, labels, num_negatives, batch_size)
+    dataset = TripletDataset(samples, labels, num_negatives, batch_size)
     train(model, dataset, epochs, optimizer)
     input_ids = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=np.int32).reshape((1, 10))
     output = model(input_ids)
@@ -86,15 +99,10 @@ def pipeline(learning_rate, batch_size, epochs, num_negatives, embedding_dim, nu
     calculate_neighbors(predicted_embeddings, output)
     validate(model, predicted_embeddings, labels)
 
-# Functional style main function definition
 def main():
     np.random.seed(42)
     pipeline(1e-4, 32, 10, 5, 101, 10, 100)
 
-if __name__ == "__main__":
-    main()
-
-# Utility functions
 def embedding_visualization(embeddings, labels):
     tsne = TSNE(n_components=2)
     reduced_embeddings = tsne.fit_transform(embeddings.numpy())
@@ -115,3 +123,6 @@ def knn_f1(embeddings, labels, k=5):
     precision = knn_precision(embeddings, labels, k)
     recall = knn_recall(embeddings, labels, k)
     return 2 * (precision * recall) / (precision + recall)
+
+if __name__ == "__main__":
+    main()
