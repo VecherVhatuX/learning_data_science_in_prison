48ed05a85550b908b08f754ca0c4af68d841d024
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2024-11-29 11:08:41 +0300

    This is the updated content for the file run_experiment.py

--- our_trainers.py
+++ our_trainers.py
@@ -5,67 +5,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
-class TripletNetwork(keras.Model):
-    def __init__(self, embedding_dim, num_features):
-        super(TripletNetwork, self).__init__()
-        self.embedding_layer = layers.Embedding(embedding_dim, num_features)
-        self.average_pooling = layers.GlobalAveragePooling1D()
-        self.flatten_layer = layers.Flatten()
-        self.dense_layer = layers.Dense(num_features)
-        self.batch_normalization = layers.BatchNormalization()
-        self.layer_normalization = layers.LayerNormalization()
+# Functional style model definition
+def create_model(embedding_dim, num_features):
+    inputs = keras.Input(shape=(10,), dtype='int32')
+    x = layers.Embedding(embedding_dim, num_features)(inputs)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(num_features)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.LayerNormalization()(x)
+    return keras.Model(inputs=inputs, outputs=x)
 
-    def call(self, inputs):
-        x = self.embedding_layer(inputs)
-        x = self.average_pooling(x)
-        x = self.flatten_layer(x)
-        x = self.dense_layer(x)
-        x = self.batch_normalization(x)
-        x = self.layer_normalization(x)
-        return x
+# Functional style loss function definition
+def triplet_loss(y_true, y_pred, margin=1.0):
+    anchor, positive, negative = y_pred
+    d_ap = tf.norm(anchor - positive, axis=-1)
+    d_an = tf.norm(anchor[:, tf.newaxis, :] - negative, axis=-1)
+    loss = tf.maximum(d_ap - tf.reduce_min(d_an, axis=-1) + margin, tf.zeros_like(d_ap))
+    return tf.reduce_mean(loss)
 
-class TripletLoss(keras.losses.Loss):
-    def __init__(self, margin=1.0):
-        super(TripletLoss, self).__init__()
-        self.margin = margin
+# Functional style dataset generator definition
+def create_dataset(samples, labels, num_negatives, batch_size):
+    def generator():
+        while True:
+            anchor_idx = np.random.choice(len(samples), size=batch_size, replace=False)
+            anchor_label = labels[anchor_idx]
+            positive_idx = np.array([np.random.choice(np.where(labels == label)[0], size=1)[0] for label in anchor_label])
+            negative_idx = np.array([np.random.choice(np.where(labels != label)[0], size=num_negatives, replace=False) for label in anchor_label])
+            yield samples[anchor_idx], samples[positive_idx], samples[negative_idx]
+    return generator
 
-    def call(self, y_true, y_pred):
-        anchor, positive, negative = y_pred
-        d_ap = tf.norm(anchor - positive, axis=-1)
-        d_an = tf.norm(anchor[:, tf.newaxis, :] - negative, axis=-1)
-        loss = tf.maximum(d_ap - tf.reduce_min(d_an, axis=-1) + self.margin, tf.zeros_like(d_ap))
-        return tf.reduce_mean(loss)
-
-class TripletDataGenerator(keras.utils.Sequence):
-    def __init__(self, samples, labels, num_negatives, batch_size, shuffle=True):
-        self.samples = samples
-        self.labels = labels
-        self.num_negatives = num_negatives
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.indices = np.arange(len(samples))
-        self.on_epoch_end()
-
-    def __len__(self):
-        return len(self.samples) // self.batch_size
-
-    def on_epoch_end(self):
-        if self.shuffle:
-            np.random.shuffle(self.indices)
-        self.anchor_idx = np.random.choice(self.indices, size=self.batch_size, replace=False)
-        self.anchor_label = self.labels[self.anchor_idx]
-        self.positive_idx = np.array([np.random.choice(np.where(self.labels == label)[0], size=1)[0] for label in self.anchor_label])
-        self.negative_idx = np.array([np.random.choice(np.where(self.labels != label)[0], size=self.num_negatives, replace=False) for label in self.anchor_label])
-
-    def __getitem__(self, idx):
-        batch_indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
-        return self.samples[batch_indices], self.samples[self.positive_idx], self.samples[self.negative_idx]
-
-def train_network(model, dataset, epochs, optimizer):
-    loss_fn = TripletLoss()
+# Functional style training function definition
+def train(model, dataset, epochs, optimizer):
+    loss_fn = lambda y_true, y_pred: triplet_loss(y_true, y_pred)
     for epoch in range(epochs):
-        dataset.on_epoch_end()
-        for batch in dataset:
+        for batch in dataset():
             with tf.GradientTape() as tape:
                 anchor, positive, negative = batch
                 anchor_embeddings = model(anchor)
@@ -76,7 +50,8 @@ def train_network(model, dataset, epochs, optimizer):
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
             print(f'Epoch: {epoch+1}, Loss: {loss.numpy()}')
 
-def validate_network(model, embeddings, labels, k=5):
+# Functional style validation function definition
+def validate(model, embeddings, labels, k=5):
     predicted_embeddings = model(embeddings)
     print("Validation KNN Accuracy:", knn_accuracy(predicted_embeddings, labels, k))
     print("Validation KNN Precision:", knn_precision(predicted_embeddings, labels, k))
@@ -84,29 +59,42 @@ def validate_network(model, embeddings, labels, k=5):
     print("Validation KNN F1-score:", knn_f1(predicted_embeddings, labels, k))
     embedding_visualization(predicted_embeddings, labels)
 
-def create_samples(size):
-    return np.random.randint(0, 100, (size, 10))
+# Functional style distance calculation function definition
+def calculate_distances(output):
+    print(tf.norm(output - output, axis=-1).numpy())
+    print(tf.reduce_sum(output * output, axis=-1) / (tf.norm(output, axis=-1) * tf.norm(output, axis=-1)).numpy())
+    print(1 - tf.reduce_sum(output * output, axis=-1) / (tf.norm(output, axis=-1) * tf.norm(output, axis=-1)).numpy())
 
-def create_labels(size):
-    return np.random.randint(0, 2, (size,))
+# Functional style nearest neighbors calculation function definition
+def calculate_neighbors(predicted_embeddings, output, k=5):
+    print(tf.argsort(tf.norm(predicted_embeddings - output, axis=-1), direction='ASCENDING')[:, :k].numpy())
+    print(tf.argsort(tf.reduce_sum(predicted_embeddings * output, axis=-1) / (tf.norm(predicted_embeddings, axis=-1) * tf.norm(output, axis=-1)), direction='DESCENDING')[:, :k].numpy())
 
-def distance(embedding1, embedding2):
-    return tf.norm(embedding1 - embedding2, axis=-1)
+# Functional style pipeline definition
+def pipeline(learning_rate, batch_size, epochs, num_negatives, embedding_dim, num_features, size):
+    samples = np.random.randint(0, 100, (size, 10))
+    labels = np.random.randint(0, 2, (size,))
+    model = create_model(embedding_dim, num_features)
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    dataset = create_dataset(samples, labels, num_negatives, batch_size)
+    train(model, dataset, epochs, optimizer)
+    input_ids = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=np.int32).reshape((1, 10))
+    output = model(input_ids)
+    model.save_weights("triplet_model.h5")
+    predicted_embeddings = model(samples)
+    calculate_distances(output)
+    calculate_neighbors(predicted_embeddings, output)
+    validate(model, predicted_embeddings, labels)
 
-def similarity(embedding1, embedding2):
-    return tf.reduce_sum(embedding1 * embedding2, axis=-1) / (tf.norm(embedding1, axis=-1) * tf.norm(embedding2, axis=-1))
+# Functional style main function definition
+def main():
+    np.random.seed(42)
+    pipeline(1e-4, 32, 10, 5, 101, 10, 100)
 
-def cosine_distance(embedding1, embedding2):
-    return 1 - similarity(embedding1, embedding2)
+if __name__ == "__main__":
+    main()
 
-def nearest_neighbors(embeddings, target_embedding, k=5):
-    distances = distance(embeddings, target_embedding)
-    return tf.argsort(distances, direction='ASCENDING')[:k]
-
-def similar_embeddings(embeddings, target_embedding, k=5):
-    similarities = similarity(embeddings, target_embedding)
-    return tf.argsort(similarities, direction='DESCENDING')[:k]
-
+# Utility functions
 def embedding_visualization(embeddings, labels):
     tsne = TSNE(n_components=2)
     reduced_embeddings = tsne.fit_transform(embeddings.numpy())
@@ -115,67 +103,15 @@ def embedding_visualization(embeddings, labels):
     plt.show()
 
 def knn_accuracy(embeddings, labels, k=5):
-    return tf.reduce_mean(tf.cast(tf.reduce_any(tf.equal(labels[tf.argsort(distance(embeddings, embeddings), axis=1)[:, 1:k+1]], labels[:, tf.newaxis]), tf.float32), axis=1))
+    return tf.reduce_mean(tf.cast(tf.reduce_any(tf.equal(labels[tf.argsort(tf.norm(embeddings - embeddings, axis=1), axis=1)[:, 1:k+1]], labels[:, tf.newaxis]), tf.float32), axis=1))
 
 def knn_precision(embeddings, labels, k=5):
-    return tf.reduce_mean(tf.reduce_sum(tf.cast(tf.equal(labels[tf.argsort(distance(embeddings, embeddings), axis=1)[:, 1:k+1]], labels[:, tf.newaxis]), tf.float32), axis=1) / k)
+    return tf.reduce_mean(tf.reduce_sum(tf.cast(tf.equal(labels[tf.argsort(tf.norm(embeddings - embeddings, axis=1), axis=1)[:, 1:k+1]], labels[:, tf.newaxis]), tf.float32), axis=1) / k)
 
 def knn_recall(embeddings, labels, k=5):
-    return tf.reduce_mean(tf.reduce_sum(tf.cast(tf.equal(labels[tf.argsort(distance(embeddings, embeddings), axis=1)[:, 1:k+1]], labels[:, tf.newaxis]), tf.float32), axis=1) / tf.reduce_sum(tf.cast(tf.equal(labels, labels[:, tf.newaxis]), tf.float32), axis=1))
+    return tf.reduce_mean(tf.reduce_sum(tf.cast(tf.equal(labels[tf.argsort(tf.norm(embeddings - embeddings, axis=1), axis=1)[:, 1:k+1]], labels[:, tf.newaxis]), tf.float32), axis=1) / tf.reduce_sum(tf.cast(tf.equal(labels, labels[:, tf.newaxis]), tf.float32), axis=1))
 
 def knn_f1(embeddings, labels, k=5):
     precision = knn_precision(embeddings, labels, k)
     recall = knn_recall(embeddings, labels, k)
     return 2 * (precision * recall) / (precision + recall)
-
-def build_optimizer(model, learning_rate):
-    return keras.optimizers.Adam(learning_rate=learning_rate)
-
-def train(model, dataset, epochs, optimizer):
-    train_network(model, dataset, epochs, optimizer)
-
-def validate(model, embeddings, labels, k=5):
-    validate_network(model, embeddings, labels, k)
-
-def test(model, input_ids):
-    return model(input_ids)
-
-def save_model(model):
-    model.save_weights("triplet_model.h5")
-
-def create_model(embedding_dim, num_features):
-    return TripletNetwork(embedding_dim, num_features)
-
-def create_dataset(samples, labels, num_negatives, batch_size):
-    return TripletDataGenerator(samples, labels, num_negatives, batch_size)
-
-def calculate_distances(output):
-    print(distance(output, output).numpy())
-    print(similarity(output, output).numpy())
-    print(cosine_distance(output, output).numpy())
-
-def calculate_neighbors(predicted_embeddings, output, k=5):
-    print(nearest_neighbors(predicted_embeddings, output, k).numpy())
-    print(similar_embeddings(predicted_embeddings, output, k).numpy())
-
-def pipeline(learning_rate, batch_size, epochs, num_negatives, embedding_dim, num_features, size):
-    samples = create_samples(size)
-    labels = create_labels(size)
-    model = create_model(embedding_dim, num_features)
-    optimizer = build_optimizer(model, learning_rate)
-    dataset = create_dataset(samples, labels, num_negatives, batch_size)
-    train(model, dataset, epochs, optimizer)
-    input_ids = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=np.int32).reshape((1, 10))
-    output = test(model, input_ids)
-    save_model(model)
-    predicted_embeddings = model(samples)
-    calculate_distances(output)
-    calculate_neighbors(predicted_embeddings, output)
-    validate(model, predicted_embeddings, labels)
-
-def main():
-    np.random.seed(42)
-    pipeline(1e-4, 32, 10, 5, 101, 10, 100)
-
-if __name__ == "__main__":
-    main()
