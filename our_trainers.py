88284a45ba747036a8253fc084208b3be9c44520
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2024-11-29 05:12:36 +0300

    This is the updated content for the file run_experiment.py

--- our_trainers.py
+++ our_trainers.py
@@ -6,29 +6,35 @@ from sklearn.manifold import TSNE
 class TripletModel(tf.keras.Model):
     def __init__(self, embedding_dim, num_features):
         super(TripletModel, self).__init__()
-        self.model = tf.keras.Sequential([
-            tf.keras.layers.Embedding(embedding_dim, num_features),
-            tf.keras.layers.GlobalAveragePooling1D(),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(num_features),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LayerNormalization()
-        ])
+        self.embedding = tf.keras.layers.Embedding(embedding_dim, num_features)
+        self.global_pool = tf.keras.layers.GlobalAveragePooling1D()
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense = tf.keras.layers.Dense(num_features)
+        self.batch_norm = tf.keras.layers.BatchNormalization()
+        self.layer_norm = tf.keras.layers.LayerNormalization()
 
     def call(self, inputs):
-        return self.model(inputs)
+        x = self.embedding(inputs)
+        x = self.global_pool(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = self.batch_norm(x)
+        x = self.layer_norm(x)
+        return x
 
-class TripletLoss:
+class TripletLoss(tf.keras.losses.Loss):
     def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
         self.margin = margin
 
-    def __call__(self, anchor, positive, negative):
+    def call(self, y_true, y_pred):
+        anchor, positive, negative = y_pred
         d_ap = tf.norm(anchor - positive, axis=-1)
         d_an = tf.norm(anchor[:, tf.newaxis, :] - negative, axis=-1)
         loss = tf.maximum(d_ap - tf.reduce_min(d_an, axis=-1) + self.margin, tf.zeros_like(d_ap))
         return tf.reduce_mean(loss)
 
-class Dataset(tf.keras.utils.Sequence):
+class TripletDataset(tf.keras.utils.Sequence):
     def __init__(self, samples, labels, num_negatives, batch_size, shuffle=True):
         self.samples = samples
         self.labels = labels
@@ -36,6 +42,7 @@ class Dataset(tf.keras.utils.Sequence):
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.indices = np.arange(len(samples))
+        self.on_epoch_end()
 
     def __len__(self):
         return len(self.samples) // self.batch_size
@@ -43,25 +50,26 @@ class Dataset(tf.keras.utils.Sequence):
     def on_epoch_end(self):
         if self.shuffle:
             np.random.shuffle(self.indices)
+        self.anchor_idx = np.random.choice(self.indices, size=self.batch_size, replace=False)
+        self.anchor_label = self.labels[self.anchor_idx]
+        self.positive_idx = np.array([np.random.choice(np.where(self.labels == label)[0], size=1)[0] for label in self.anchor_label])
+        self.negative_idx = np.array([np.random.choice(np.where(self.labels != label)[0], size=self.num_negatives, replace=False) for label in self.anchor_label])
 
     def __getitem__(self, idx):
         batch_indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
-        anchor_idx = np.random.choice(batch_indices, size=self.batch_size, replace=False)
-        anchor_label = self.labels[anchor_idx]
-        positive_idx = np.array([np.random.choice(np.where(self.labels == label)[0], size=1)[0] for label in anchor_label])
-        negative_idx = np.array([np.random.choice(np.where(self.labels != label)[0], size=self.num_negatives, replace=False) for label in anchor_label])
-        return self.samples[anchor_idx], self.samples[positive_idx], self.samples[negative_idx]
+        return self.samples[batch_indices], self.samples[self.positive_idx], self.samples[self.negative_idx]
 
-def train(model, dataset, epochs, optimizer, criterion):
+def train(model, dataset, epochs, optimizer):
+    loss_fn = TripletLoss()
     for epoch in range(epochs):
         dataset.on_epoch_end()
         for batch in dataset:
-            anchor, positive, negative = batch
             with tf.GradientTape() as tape:
+                anchor, positive, negative = batch
                 anchor_embeddings = model(anchor)
                 positive_embeddings = model(positive)
                 negative_embeddings = model(negative)
-                loss = criterion(anchor_embeddings, positive_embeddings, negative_embeddings)
+                loss = loss_fn(None, (anchor_embeddings, positive_embeddings, negative_embeddings))
             gradients = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
             print(f'Epoch: {epoch+1}, Loss: {loss.numpy()}')
@@ -119,10 +127,9 @@ def pipeline(learning_rate, batch_size, epochs, num_negatives, embedding_dim, nu
     samples = np.random.randint(0, 100, (100, 10))
     labels = np.random.randint(0, 2, (100,))
     model = TripletModel(embedding_dim, num_features)
-    criterion = TripletLoss()
     optimizer = build_optimizer(model, learning_rate)
-    dataset = Dataset(samples, labels, num_negatives, batch_size)
-    train(model, dataset, epochs, optimizer, criterion)
+    dataset = TripletDataset(samples, labels, num_negatives, batch_size)
+    train(model, dataset, epochs, optimizer)
     input_ids = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=np.int32).reshape((1, 10))
     output = model(input_ids)
     model.save_weights("triplet_model.h5")
