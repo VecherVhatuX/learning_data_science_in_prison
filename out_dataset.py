1f46250f0e5e332a08c3fcbd1b50de81da2fb381
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2025-03-12 13:43:37 +0300

    This is the updated content for the file run_experiment.py

--- out_dataset.py
+++ out_dataset.py
@@ -7,69 +7,69 @@ from tensorflow.keras import layers, models
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 
-def extract_texts(triplet_data):
-    return [entry[key] for entry in triplet_data for key in ['anchor', 'positive', 'negative']]
+def get_texts(triplet_data):
+    return [item for entry in triplet_data for item in (entry['anchor'], entry['positive'], entry['negative'])]
 
-def sequence_conversion(tokenizer, entry):
+def convert_to_sequences(tokenizer, entry):
     return {
         'anchor_seq': tokenizer.transform([entry['anchor']])[0],
         'positive_seq': tokenizer.transform([entry['positive']])[0],
         'negative_seq': tokenizer.transform([entry['negative']])[0]
     }
 
-class TripletDataLoader(tf.keras.utils.Sequence):
+class TripletDataGenerator(tf.keras.utils.Sequence):
     def __init__(self, triplet_data, max_length, batch_size):
         self.triplet_data = triplet_data
         self.max_length = max_length
         self.batch_size = batch_size
         self.tokenizer = LabelEncoder()
-        self.tokenizer.fit(extract_texts(triplet_data))
+        self.tokenizer.fit(get_texts(triplet_data))
 
     def __len__(self):
-        return -(-len(self.triplet_data) // self.batch_size)
+        return len(self.triplet_data) // self.batch_size + (len(self.triplet_data) % self.batch_size > 0)
 
-    def __getitem__(self, index):
-        batch = self.triplet_data[index * self.batch_size:(index + 1) * self.batch_size]
-        return np.array([sequence_conversion(self.tokenizer, entry) for entry in batch])
+    def __getitem__(self, idx):
+        batch = self.triplet_data[idx * self.batch_size : (idx + 1) * self.batch_size]
+        return np.array([convert_to_sequences(self.tokenizer, entry) for entry in batch])
 
-    def shuffle(self):
+    def shuffle_data(self):
         random.shuffle(self.triplet_data)
 
-    def prepare_next_epoch(self):
-        self.shuffle()
+    def reset_for_epoch(self):
+        self.shuffle_data()
 
-def read_json_data(file_path, directory_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    instance_mapping = {item['instance_id']: item['problem_statement'] for item in data}
-    snippet_files = [
+def load_json(file_path, base_dir):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    mapping = {item['instance_id']: item['problem_statement'] for item in data}
+    snippet_paths = [
         (folder, os.path.join(folder, 'snippet.json'))
-        for folder in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, folder))
+        for folder in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, folder))
     ]
-    return instance_mapping, snippet_files
+    return mapping, snippet_paths
 
-def create_triplet_data(instance_mapping, snippet_files):
-    bug_snippets, non_bug_snippets = zip(*(map(lambda file: json.load(open(file)), snippet_files)))
-    bug_snippets = [s['snippet'] for s in bug_snippets if s.get('is_bug') and s['snippet']]
-    non_bug_snippets = [s['snippet'] for s in non_bug_snippets if not s.get('is_bug') and s['snippet']]
-    return build_triplet_structure(instance_mapping, snippet_files, bug_snippets, non_bug_snippets)
+def generate_triplet_data(instance_mapping, snippet_paths):
+    bug_snips, non_bug_snips = zip(*(map(lambda path: json.load(open(path)), snippet_paths)))
+    bug_snips = [s['snippet'] for s in bug_snips if s.get('is_bug') and s['snippet']]
+    non_bug_snips = [s['snippet'] for s in non_bug_snips if not s.get('is_bug') and s['snippet']]
+    return create_triplet_structure(instance_mapping, snippet_paths, bug_snips, non_bug_snips)
 
-def build_triplet_structure(instance_mapping, snippet_files, bug_snippets, non_bug_snippets):
+def create_triplet_structure(instance_mapping, snippet_paths, bug_snips, non_bug_snips):
     return [
         {
             'anchor': instance_mapping[os.path.basename(folder)],
-            'positive': pos_snippet,
-            'negative': random.choice(non_bug_snippets)
+            'positive': pos_snip,
+            'negative': random.choice(non_bug_snips)
         }
-        for folder, _ in snippet_files
-        for pos_snippet in bug_snippets
+        for folder, _ in snippet_paths
+        for pos_snip in bug_snips
     ]
 
-class TripletNN(models.Model):
-    def __init__(self, vocab_size, embed_dim):
-        super(TripletNN, self).__init__()
-        self.embedding_layer = layers.Embedding(vocab_size, embed_dim)
-        self.fc_layers = models.Sequential([
+class TripletNetwork(models.Model):
+    def __init__(self, vocab_size, embedding_dim):
+        super().__init__()
+        self.embedding = layers.Embedding(vocab_size, embedding_dim)
+        self.dense_layers = models.Sequential([
             layers.Dense(128, activation='relu'),
             layers.BatchNormalization(),
             layers.Dropout(0.2),
@@ -78,92 +78,92 @@ class TripletNN(models.Model):
 
     def call(self, inputs):
         anchor, positive, negative = inputs
-        return (self.fc_layers(self.embedding_layer(anchor)), 
-                self.fc_layers(self.embedding_layer(positive)), 
-                self.fc_layers(self.embedding_layer(negative)))
+        return (self.dense_layers(self.embedding(anchor)),
+                self.dense_layers(self.embedding(positive)),
+                self.dense_layers(self.embedding(negative)))
 
-def compute_triplet_loss(y_true, y_pred):
+def triplet_loss(y_true, y_pred):
     anchor_embeds, positive_embeds, negative_embeds = y_pred
-    return tf.reduce_mean(tf.maximum(0.2 + tf.norm(anchor_embeds - positive_embeds, axis=1) - 
+    return tf.reduce_mean(tf.maximum(0.2 + tf.norm(anchor_embeds - positive_embeds, axis=1) -
                                       tf.norm(anchor_embeds - negative_embeds, axis=1), 0))
 
-def train_triplet_model(model, train_loader, test_loader, epochs):
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5), loss=compute_triplet_loss)
-    training_losses, validation_losses, training_accuracies = [], [], []
+def train_model(model, train_gen, valid_gen, num_epochs):
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5), loss=triplet_loss)
+    train_losses, val_losses, train_accuracies = [], [], []
 
-    for epoch in range(epochs):
-        model.fit(train_loader, epochs=1, verbose=1)
-        train_loss = model.evaluate(train_loader, verbose=0)
-        training_losses.append(train_loss)
+    for epoch in range(num_epochs):
+        model.fit(train_gen, epochs=1, verbose=1)
+        train_loss = model.evaluate(train_gen, verbose=0)
+        train_losses.append(train_loss)
 
-        val_loss, accuracy = validate_model(model, test_loader)
-        validation_losses.append(val_loss)
-        training_accuracies.append(accuracy)
+        val_loss, accuracy = evaluate_model(model, valid_gen)
+        val_losses.append(val_loss)
+        train_accuracies.append(accuracy)
         print(f'Epoch {epoch + 1}, Train Loss: {train_loss}, Validation Loss: {val_loss}, Validation Accuracy: {accuracy}')
 
-    return training_losses, validation_losses, training_accuracies
+    return train_losses, val_losses, train_accuracies
 
-def validate_model(model, test_loader):
-    test_loss = model.evaluate(test_loader)
-    correct_predictions = sum(count_correct_preds(model(batch['anchor_seq']), 
-                                                  model(batch['positive_seq']), 
+def evaluate_model(model, valid_gen):
+    loss = model.evaluate(valid_gen)
+    correct_preds = sum(count_correct_predictions(model(batch['anchor_seq']),
+                                                  model(batch['positive_seq']),
                                                   model(batch['negative_seq'])) 
-                              for batch in test_loader)
+                        for batch in valid_gen)
 
-    accuracy = correct_predictions / len(test_loader.triplet_data)
-    return test_loss, accuracy
+    accuracy = correct_preds / len(valid_gen.triplet_data)
+    return loss, accuracy
 
-def count_correct_preds(anchor_out, positive_out, negative_out):
-    pos_similarity = tf.reduce_sum(anchor_out * positive_out, axis=1)
-    neg_similarity = tf.reduce_sum(anchor_out * negative_out, axis=1)
-    return tf.reduce_sum(pos_similarity > neg_similarity).numpy()
+def count_correct_predictions(anchor_output, positive_output, negative_output):
+    positive_similarity = tf.reduce_sum(anchor_output * positive_output, axis=1)
+    negative_similarity = tf.reduce_sum(anchor_output * negative_output, axis=1)
+    return tf.reduce_sum(positive_similarity > negative_similarity).numpy()
 
-def visualize_results(train_losses, val_losses, train_accs, val_accs):
+def plot_results(train_losses, val_losses, train_accuracies, val_accuracies):
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
-    plt.plot(train_losses, label='Training Loss')
+    plt.plot(train_losses, label='Train Loss')
     plt.plot(val_losses, label='Validation Loss')
-    plt.title('Loss Over Epochs')
+    plt.title('Loss Throughout Epochs')
     plt.xlabel('Epochs')
-    plt.ylabel('Loss Value')
+    plt.ylabel('Loss')
     plt.legend()
 
     plt.subplot(1, 2, 2)
-    plt.plot(train_accs, label='Training Accuracy')
-    plt.plot(val_accs, label='Validation Accuracy')
-    plt.title('Accuracy Over Epochs')
+    plt.plot(train_accuracies, label='Train Accuracy')
+    plt.plot(val_accuracies, label='Validation Accuracy')
+    plt.title('Accuracy Throughout Epochs')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
     plt.show()
 
-def save_trained_model(model, file_path):
-    model.save(file_path)
-    print(f'Model saved at {file_path}')
+def save_model(model, path):
+    model.save(path)
+    print(f'Model saved at {path}')
 
-def load_trained_model(file_path):
-    model = models.load_model(file_path)
+def load_model(path):
+    model = models.load_model(path)
     model.evaluate()
-    print(f'Model loaded from {file_path}')
+    print(f'Model loaded from {path}')
     return model
 
-def run_main():
-    data_file = 'datasets/SWE-bench_oracle.npy'
-    snippets_directory = 'datasets/10_10_after_fix_pytest'
+def main():
+    data_path = 'datasets/SWE-bench_oracle.npy'
+    snippets_path = 'datasets/10_10_after_fix_pytest'
     
-    instance_mapping, snippet_files = read_json_data(data_file, snippets_directory)
-    triplet_data = create_triplet_data(instance_mapping, snippet_files)
+    instance_map, snippet_files = load_json(data_path, snippets_path)
+    triplet_data = generate_triplet_data(instance_map, snippet_files)
     train_data, valid_data = np.array_split(np.array(triplet_data), 2)
     
-    train_loader = TripletDataLoader(train_data.tolist(), max_length=512, batch_size=32)
-    test_loader = TripletDataLoader(valid_data.tolist(), max_length=512, batch_size=32)
+    train_gen = TripletDataGenerator(train_data.tolist(), max_length=512, batch_size=32)
+    valid_gen = TripletDataGenerator(valid_data.tolist(), max_length=512, batch_size=32)
     
-    model = TripletNN(vocab_size=len(train_loader.tokenizer.classes_) + 1, embed_dim=128)
+    model = TripletNetwork(vocab_size=len(train_gen.tokenizer.classes_) + 1, embedding_dim=128)
     
-    training_losses, validation_losses, training_accuracies = train_triplet_model(model, train_loader, test_loader, epochs=5)
-    visualize_results(training_losses, validation_losses, training_accuracies, [])
+    train_losses, validation_losses, training_accuracies = train_model(model, train_gen, valid_gen, epochs=5)
+    plot_results(train_losses, validation_losses, training_accuracies, [])
 
-    save_trained_model(model, 'triplet_model.h5')
+    save_model(model, 'triplet_model.h5')
 
 if __name__ == "__main__":
-    run_main()
+    main()
