a9623847629a6ad2c3644354f7b41b3aa8df105c
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2025-03-12 12:48:15 +0300

    This is the updated content for the file run_experiment.py

--- our_trainers.py
+++ our_trainers.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
@@ -8,7 +8,7 @@ import random
 
 class NeuralEmbedder(tf.keras.Model):
     def __init__(self, embedding_dim, feature_dim):
-        super(NeuralEmbedder, self).__init__()
+        super().__init__()
         self.embedding = layers.Embedding(embedding_dim, feature_dim)
         self.pooling = layers.GlobalAveragePooling1D()
         self.dense = layers.Dense(feature_dim)
@@ -23,70 +23,66 @@ class NeuralEmbedder(tf.keras.Model):
         return self.layer_norm(x)
 
 
-class CustomTripletDataset(tf.keras.utils.Sequence):
-    def __init__(self, data_samples, data_labels, num_negatives, batch_size=32):
-        self.data_samples = data_samples
-        self.data_labels = data_labels
+class TripletDataset(tf.keras.utils.Sequence):
+    def __init__(self, samples, labels, num_negatives, batch_size=32):
+        self.samples = samples
+        self.labels = labels
         self.num_negatives = num_negatives
         self.batch_size = batch_size
-        self.indices = np.arange(len(self.data_samples))
+        self.indices = np.arange(len(self.samples))
 
     def __len__(self):
-        return int(np.ceil(len(self.data_samples) / self.batch_size))
+        return int(np.ceil(len(self.samples) / self.batch_size))
 
     def __getitem__(self, idx):
         batch_indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
         anchors, positives, negatives = [], [], []
         
         for anchor_idx in batch_indices:
-            anchor = self.data_samples[anchor_idx]
-            anchor_label = self.data_labels[anchor_idx]
-            pos_idx = random.choice(np.where(self.data_labels == anchor_label)[0])
-            negative_indices = random.sample(np.where(self.data_labels != anchor_label)[0].tolist(), self.num_negatives)
+            anchor = self.samples[anchor_idx]
+            anchor_label = self.labels[anchor_idx]
+            pos_idx = random.choice(np.where(self.labels == anchor_label)[0])
+            negative_indices = random.sample(np.where(self.labels != anchor_label)[0].tolist(), self.num_negatives)
             anchors.append(anchor)
-            positives.append(self.data_samples[pos_idx])
-            negatives.append([self.data_samples[i] for i in negative_indices])
+            positives.append(self.samples[pos_idx])
+            negatives.append([self.samples[i] for i in negative_indices])
 
         return np.array(anchors), np.array(positives), np.array(negatives)
 
-    def shuffle_data(self):
+    def shuffle(self):
         np.random.shuffle(self.indices)
 
 
-def build_triplet_dataset(data_samples, data_labels, num_negatives, batch_size=32):
-    return CustomTripletDataset(data_samples, data_labels, num_negatives, batch_size)
-
-
-def compute_triplet_loss(margin_value=1.0):
+def compute_triplet_loss(margin=1.0):
     def loss_fn(anchor, positive, negative_samples):
         pos_dist = tf.norm(anchor - positive, axis=1)
         neg_dist = tf.norm(tf.expand_dims(anchor, axis=1) - tf.convert_to_tensor(negative_samples), axis=2)
-        return tf.reduce_mean(tf.maximum(pos_dist - tf.reduce_min(neg_dist, axis=1) + margin_value, 0.0))
+        return tf.reduce_mean(tf.maximum(pos_dist - tf.reduce_min(neg_dist, axis=1) + margin, 0.0))
     return loss_fn
 
 
-def train_neural_network(model, data_set, epochs):
+def train_model(model, dataset, epochs):
     optimizer = tf.keras.optimizers.Adam()
     loss_function = compute_triplet_loss()
     model.compile(optimizer=optimizer, loss=lambda y_true, y_pred: loss_function(y_pred[0], y_pred[1], y_pred[2]))
 
     for epoch in range(epochs):
-        data_set.shuffle_data()
-        for anchors, positives, negatives in data_set:
+        dataset.shuffle()
+        for anchors, positives, negatives in dataset:
             with tf.GradientTape() as tape:
                 loss_value = model([anchors, positives, negatives])
             gradients = tape.gradient(loss_value, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 
-def evaluate_model(model, data_samples, data_labels, k_neighbors=5):
+def evaluate(model, data_samples, data_labels, k_neighbors=5):
     embeddings = model.predict(data_samples)
     metrics = compute_knn_metrics(embeddings, data_labels, k_neighbors)
-    display_evaluation_metrics(metrics)
+    display_metrics(metrics)
     visualize_embeddings(embeddings, data_labels)
 
 
-def display_evaluation_metrics(metrics):
+def display_metrics(metrics):
     print(f"Validation KNN Accuracy: {metrics[0]}")
     print(f"Validation KNN Precision: {metrics[1]}")
     print(f"Validation KNN Recall: {metrics[2]}")
@@ -101,7 +97,7 @@ def save_model(model, file_path):
     model.save(file_path)
 
 
-def extract_model_embeddings(model, data_samples):
+def extract_embeddings(model, data_samples):
     return model.predict(data_samples)
 
 
@@ -128,7 +124,7 @@ def compute_knn_metrics(embeddings, labels, k_neighbors=5):
     return accuracy, precision, recall, f1_score
 
 
-def plot_training_loss(loss_history):
+def plot_loss(loss_history):
     plt.figure(figsize=(10, 5))
     plt.plot(loss_history, label='Loss', color='blue')
     plt.title('Training Loss Progression')
@@ -140,21 +136,23 @@ def plot_training_loss(loss_history):
 
 def run_training_pipeline(learning_rate, batch_size, num_epochs, num_negatives, embedding_dim, feature_dim, data_size):
     samples, labels = create_random_data(data_size)
-    triplet_dataset = build_triplet_dataset(samples, labels, num_negatives, batch_size)
+    triplet_dataset = TripletDataset(samples, labels, num_negatives, batch_size)
     model = NeuralEmbedder(embedding_dim, feature_dim)
-    train_neural_network(model, triplet_dataset, num_epochs)
+    train_model(model, triplet_dataset, num_epochs)
     input_ids = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
     model(input_ids)
     save_model(model, "triplet_model.h5")
-    predicted_embeddings = extract_model_embeddings(model, samples)
-    evaluate_model(model, samples, labels)
+    predicted_embeddings = extract_embeddings(model, samples)
+    evaluate(model, samples, labels)
+
 
 def load_model(file_path):
     return tf.keras.models.load_model(file_path)
 
+
 def generate_additional_data(size):
-    """Generates additional data samples for model evaluation."""
     return np.random.randn(size, 10), np.random.randint(0, 2, size)
+
 
 if __name__ == "__main__":
     run_training_pipeline(1e-4, 32, 10, 5, 101, 10, 100)
