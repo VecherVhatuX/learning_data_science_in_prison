59f11258a55fafe667b4be5c061f88a0dc020307
VecherVhatu <164204489+VecherVhatuX@users.noreply.github.com>
2025-03-13 20:47:55 +0300

    This is the updated content for the file out_dataset.py

--- out_dataset.py
+++ out_dataset.py
@@ -7,66 +7,66 @@ from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 from tensorflow.keras import layers, optimizers, losses
 
-def gather_texts(triplet_data):
-    return [text for item in triplet_data for text in (item['anchor'], item['positive'], item['negative'])]
+def collect_texts(triplet_info):
+    return [text for entry in triplet_info for text in (entry['anchor'], entry['positive'], entry['negative'])]
 
-def tokenize_sequences(tokenizer, data_item):
+def encode_sequences(encoder, data_entry):
     return {
-        'anchor_seq': tf.convert_to_tensor(tokenizer.transform([data_item['anchor']])[0]),
-        'positive_seq': tf.convert_to_tensor(tokenizer.transform([data_item['positive']])[0]),
-        'negative_seq': tf.convert_to_tensor(tokenizer.transform([data_item['negative']])[0])
+        'anchor_seq': tf.convert_to_tensor(encoder.transform([data_entry['anchor']])[0]),
+        'positive_seq': tf.convert_to_tensor(encoder.transform([data_entry['positive']])[0]),
+        'negative_seq': tf.convert_to_tensor(encoder.transform([data_entry['negative']])[0])
     }
 
-def shuffle_data(data_samples):
-    random.shuffle(data_samples)
-    return data_samples
+def randomize_data(data_entries):
+    random.shuffle(data_entries)
+    return data_entries
 
-def generate_triplets(instance_dict, bug_samples, non_bug_samples):
+def create_triplets(instance_map, bug_entries, non_bug_entries):
     return [
         {
-            'anchor': instance_dict[os.path.basename(folder)],
-            'positive': bug_sample,
-            'negative': random.choice(non_bug_samples)
+            'anchor': instance_map[os.path.basename(dir)],
+            'positive': bug_entry,
+            'negative': random.choice(non_bug_entries)
         }
-        for folder, _ in snippet_files
-        for bug_sample in bug_samples
+        for dir, _ in snippet_paths
+        for bug_entry in bug_entries
     ]
 
-def load_json_data(file_path, root_dir):
+def load_json(file_path, base_dir):
     with open(file_path, 'r') as f:
-        json_content = json.load(f)
-    instance_dict = {entry['instance_id']: entry['problem_statement'] for entry in json_content}
-    snippet_files = [(folder, os.path.join(root_dir, 'snippet.json')) for folder in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, folder))]
-    return instance_dict, snippet_files
+        json_data = json.load(f)
+    instance_map = {item['instance_id']: item['problem_statement'] for item in json_data}
+    snippet_paths = [(dir, os.path.join(base_dir, 'snippet.json')) for dir in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, dir))]
+    return instance_map, snippet_paths
 
-def prepare_triplet_dataset(instance_dict, snippet_files):
-    bug_samples, non_bug_samples = zip(*[json.load(open(path)) for path in snippet_files])
-    return generate_triplets(instance_dict, bug_samples, non_bug_samples)
+def prepare_dataset(instance_map, snippet_paths):
+    bug_entries, non_bug_entries = zip(*[json.load(open(path)) for path in snippet_paths])
+    return create_triplets(instance_map, bug_entries, non_bug_entries)
 
 class TripletData:
-    def __init__(self, triplet_data):
-        self.triplet_data = triplet_data
-        self.tokenizer = LabelEncoder().fit(gather_texts(triplet_data))
+    def __init__(self, triplet_info):
+        self.triplet_info = triplet_info
+        self.encoder = LabelEncoder().fit(collect_texts(triplet_info))
 
-    def get_samples(self):
-        return self.triplet_data
+    def get_entries(self):
+        return self.triplet_info
 
 class TripletDataset(tf.data.Dataset):
-    def __init__(self, triplet_data):
-        self.data = TripletData(triplet_data)
-        self.samples = self.data.get_samples()
+    def __init__(self, triplet_info):
+        self.data = TripletData(triplet_info)
+        self.entries = self.data.get_entries()
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.entries)
 
-    def __getitem__(self, index):
-        return tokenize_sequences(self.data.tokenizer, self.samples[index])
+    def __getitem__(self, idx):
+        return encode_sequences(self.data.encoder, self.entries[idx])
 
 class TripletModel(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim):
+    def __init__(self, vocab_size, embed_dim):
         super(TripletModel, self).__init__()
-        self.embedding_layer = layers.Embedding(vocab_size, embedding_dim)
-        self.dense_network = tf.keras.Sequential([
+        self.embedding = layers.Embedding(vocab_size, embed_dim)
+        self.network = tf.keras.Sequential([
             layers.Dense(128, activation='relu'),
             layers.BatchNormalization(),
             layers.Dropout(0.2),
@@ -74,39 +74,39 @@ class TripletModel(tf.keras.Model):
         ])
 
     def call(self, anchor, positive, negative):
-        anchor_embed = self.dense_network(self.embedding_layer(anchor))
-        positive_embed = self.dense_network(self.embedding_layer(positive))
-        negative_embed = self.dense_network(self.embedding_layer(negative))
+        anchor_embed = self.network(self.embedding(anchor))
+        positive_embed = self.network(self.embedding(positive))
+        negative_embed = self.network(self.embedding(negative))
         return anchor_embed, positive_embed, negative_embed
 
-def calculate_loss(anchor_embeds, positive_embeds, negative_embeds):
+def compute_loss(anchor_embeds, positive_embeds, negative_embeds):
     return tf.reduce_mean(tf.maximum(0.2 + tf.norm(anchor_embeds - positive_embeds, axis=1) - tf.norm(anchor_embeds - negative_embeds, axis=1), 0))
 
-def train_model(model, train_loader, valid_loader, epochs):
+def train(model, train_loader, valid_loader, epochs):
     optimizer = optimizers.Adam(learning_rate=1e-5)
     history = []
     for _ in range(epochs):
         for batch in train_loader:
             with tf.GradientTape() as tape:
                 anchor_embeds, positive_embeds, negative_embeds = model(batch['anchor_seq'], batch['positive_seq'], batch['negative_seq'])
-                loss = calculate_loss(anchor_embeds, positive_embeds, negative_embeds)
+                loss = compute_loss(anchor_embeds, positive_embeds, negative_embeds)
             gradients = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        history.append((loss.numpy(), *evaluate_model(model, valid_loader)))
+        history.append((loss.numpy(), *evaluate(model, valid_loader)))
     return history
 
-def evaluate_model(model, valid_loader):
+def evaluate(model, valid_loader):
     loss, correct = 0, 0
     for batch in valid_loader:
         anchor_embeds, positive_embeds, negative_embeds = model(batch['anchor_seq'], batch['positive_seq'], batch['negative_seq'])
-        loss += calculate_loss(anchor_embeds, positive_embeds, negative_embeds).numpy()
-        correct += count_matches(anchor_embeds, positive_embeds, negative_embeds)
+        loss += compute_loss(anchor_embeds, positive_embeds, negative_embeds).numpy()
+        correct += count_correct(anchor_embeds, positive_embeds, negative_embeds)
     return loss / len(valid_loader), correct / len(valid_loader.dataset)
 
-def count_matches(anchor_output, positive_output, negative_output):
+def count_correct(anchor_output, positive_output, negative_output):
     return tf.reduce_sum(tf.cast(tf.reduce_sum(anchor_output * positive_output, axis=1) > tf.reduce_sum(anchor_output * negative_output, axis=1), tf.int32)).numpy()
 
-def plot_results(history):
+def plot_history(history):
     train_losses, val_losses, train_accuracies = zip(*history)
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
@@ -124,27 +124,27 @@ def plot_results(history):
     plt.legend()
     plt.show()
 
-def save_model(model, filepath):
-    model.save_weights(filepath)
-    print(f'Model saved at {filepath}')
+def save_model_weights(model, path):
+    model.save_weights(path)
+    print(f'Model saved at {path}')
 
-def load_model(model, filepath):
-    model.load_weights(filepath)
-    print(f'Model loaded from {filepath}')
+def load_model_weights(model, path):
+    model.load_weights(path)
+    print(f'Model loaded from {path}')
     return model
 
 def main():
     dataset_path = 'datasets/SWE-bench_oracle.npy'
-    snippets_directory = 'datasets/10_10_after_fix_pytest'
-    instance_dict, snippet_paths = load_json_data(dataset_path, snippets_directory)
-    triplet_data = prepare_triplet_dataset(instance_dict, snippet_paths)
-    train_data, valid_data = np.array_split(np.array(triplet_data), 2)
+    snippets_dir = 'datasets/10_10_after_fix_pytest'
+    instance_map, snippet_paths = load_json(dataset_path, snippets_dir)
+    triplet_info = prepare_dataset(instance_map, snippet_paths)
+    train_data, valid_data = np.array_split(np.array(triplet_info), 2)
     train_loader = tf.data.Dataset.from_tensor_slices(train_data.tolist()).batch(32).shuffle(len(train_data))
     valid_loader = tf.data.Dataset.from_tensor_slices(valid_data.tolist()).batch(32)
-    model = TripletModel(vocab_size=len(train_loader.dataset.data.tokenizer.classes_) + 1, embedding_dim=128)
-    history = train_model(model, train_loader, valid_loader, epochs=5)
-    plot_results(history)
-    save_model(model, 'triplet_model.h5')
+    model = TripletModel(vocab_size=len(train_loader.dataset.data.encoder.classes_) + 1, embed_dim=128)
+    history = train(model, train_loader, valid_loader, epochs=5)
+    plot_history(history)
+    save_model_weights(model, 'triplet_model.h5')
 
 if __name__ == "__main__":
     main()
